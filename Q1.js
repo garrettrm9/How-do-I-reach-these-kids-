283622aa1e7faaddbcd615f40b2bc022d459c1e1
Garrett Rasko-Martinis <garrettrasko-martinis@Garretts-Air.fios-router.home>
2018-01-08 11:54:38 -0500

    updated event listeners to increase score and advance round

--- Q1.js
+++ Q1.js
@@ -8,12 +8,25 @@ var questions = {
   answer4: '7',
 };
 
-var $score = $('.score');
-$score.text = '100';
-
 var $container = $('.container');
+var $header = $('.header');
 var $answerContainer = $('<div>').attr('id', 'answerContainer');
 $container.append($answerContainer);
+var $round = $('<div>').attr('id', 'round');
+var $score = $('<div>').attr('id', 'score');
+
+var roundNum = 1;
+var scoreTotal = 0;
+
+var roundRender = function() {
+  $header.prepend($round);
+  $round.text('Round: ' + roundNum);
+};
+
+var scoreRender = function() {
+  $header.append($score);
+  $score.text('Score: ' + scoreTotal);
+};
 
 var questionRender = function() {
   //create question div, pull text from questions object,
@@ -29,7 +42,7 @@ var answerRender1 = function() {
   //append new div to answerContainer div
   var $answer1 = $('<div>')
     .attr('class', 'answer')
-    .click(checkAnswer);
+    .click(gameLogic.checkAnswer);
   $answerContainer.append($answer1);
   $answer1.text(questions.answer1);
 };
@@ -37,7 +50,7 @@ var answerRender1 = function() {
 var answerRender2 = function() {
   var $answer2 = $('<div>')
     .attr('class', 'answer')
-    .click(checkAnswer);
+    .click(gameLogic.checkAnswer);
   $answerContainer.append($answer2);
   $answer2.text(questions.answer2);
 };
@@ -45,7 +58,7 @@ var answerRender2 = function() {
 var answerRender3 = function() {
   var $answer3 = $('<div>')
     .attr('class', 'answer')
-    .click(checkAnswer);
+    .click(gameLogic.checkAnswer);
   $answerContainer.append($answer3);
   $answer3.text(questions.answer3);
 };
@@ -53,14 +66,43 @@ var answerRender3 = function() {
 var answerRender4 = function() {
   var $answer4 = $('<div>')
     .attr('class', 'answer')
-    .click(checkAnswer);
+    .click(gameLogic.checkAnswer);
   $answerContainer.append($answer4);
   $answer4.text(questions.answer4);
 };
 
-var divRender = {
+var checkAnswer = function(answer) {
+  //check answer, add score if applicaple, advance round
+  //remove event listener
+  if (questions.answer1 === answer.target.innerHTML) {
+    alert('You win');
+    scoreTotal += 10;
+    roundNum += 1;
+    scoreRender();
+    roundRender();
+    $('.answer').off('click', checkAnswer);
+  } else {
+    alert('You lose');
+    roundNum += 1;
+    roundRender();
+    $('.answer').off('click', checkAnswer);
+  }
+};
+
+var nextQuestion = function() {
+  if (roundNum === 2) {
+  } else if (roundNum === 3) {
+  } else if (roundNum === 4) {
+  } else if (roundNum === 5 && scoreTotal >= 25) {
+  } else if (roundNum === 5 && scoreTotal < 25) {
+  }
+};
+
+var renderBoard = {
   //create divs for question and answers, append to DOM
   //add appropriate styling classes and click events
+  scoreRender: scoreRender,
+  roundRender: roundRender,
   questionRender: questionRender,
   answerRender1: answerRender1,
   answerRender2: answerRender2,
@@ -68,16 +110,15 @@ var divRender = {
   answerRender4: answerRender4,
 };
 
-var checkAnswer = function(answer) {
-  if (questions.answer1 === answer.target.innerHTML) {
-    alert('You win');
-  } else {
-    alert('You lose');
-  }
+var gameLogic = {
+  checkAnswer: checkAnswer,
+  nextQuestion: nextQuestion,
 };
 
-divRender.questionRender();
-divRender.answerRender1();
-divRender.answerRender2();
-divRender.answerRender3();
-divRender.answerRender4();
+renderBoard.roundRender();
+renderBoard.scoreRender();
+renderBoard.questionRender();
+renderBoard.answerRender1();
+renderBoard.answerRender2();
+renderBoard.answerRender3();
+renderBoard.answerRender4();
